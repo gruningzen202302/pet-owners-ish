7beb5fa2361054d3e2944e00873adceb2ca87992
vir-thikpad <gruningzen202302@gmail.com>
2023-08-12 15:15:21 -0300

    feat: add finc to add pet owner

--- src/pet_owners/core.clj
+++ src/pet_owners/core.clj
@@ -1,6 +1,14 @@
-(ns pet-owners.core)
+(ns pet-owners.core
+  (:require [datomic.api :as d]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def conn nil)
+
+(defn add-pet-owner [owner-name]
+  @(d/transact conn [{:db/id (d/tempid :db.part/user)
+                      :owner/name owner-name}]))
+
+(defn find-all-owners []
+  (d/q '[:find ?owner-name
+         :where [_ :owner/name ?owner-name]]
+       (d/db conn)))
+
